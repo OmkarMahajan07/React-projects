9b49fca2a0615cfb5757afa688393c3ac9529270
omkar <omkarmahajan1702@gmail.com>
2025-11-20 12:45:03 +0530

    First upload

---
+++ Project_1/src/App.jsx
@@ -0,0 +1,16 @@
+
+const App = () => {
+  return (
+    <>
+      <div id='line1'>
+        <h1>Hello, World!</h1>
+        <h2>Good morning</h2>
+      </div>
+      <div id = 'line3'>
+        <h2>div 2 Line 2</h2>
+      </div>
+    </>
+  );
+}
+
+export default App
